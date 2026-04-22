0c88d44a5a381b996da7c70930de82f6f0f73c62
Vladislav Kryshtop <vladkryshtop@gmail.com>
2026-04-22 03:16:20 +0300

    add CI

--- backend/docker/app.sh
+++ backend/docker/app.sh
@@ -2,4 +2,7 @@
 
 alembic upgrade head
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn main:app \
+  --workers 4 \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --bind=0.0.0.0:8000
