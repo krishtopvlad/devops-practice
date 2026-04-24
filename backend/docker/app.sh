ca7818a601d597314393a1b139f3a6bedde89aa2
Vladislav Kryshtop <vladkryshtop@gmail.com>
2026-04-24 22:40:20 +0300

    fix race

--- backend/docker/app.sh
+++ backend/docker/app.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Waiting for database..."
+
+until pg_isready -h db -p 5432; do
+  echo "Database is not ready yet..."
+  sleep 2
+done
+
+echo "Database is ready!"
+
+
 alembic upgrade head
 
 gunicorn main:app \
